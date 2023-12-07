7331532f5e2a88faf3c887a7b6c1f12b23aeddec
Eshka666 <g-666@tyt.by>
2023-12-08 00:26:31 +0300

    lesson 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
+import { TaskType, Todolist } from './Todolist';
 
-function App() {
+ function App() {
+ 
+  let tasks1: Array<TaskType> =[
+    {id:1, title:"CSS", isDone: true},
+    {id:2, title:"Js", isDone: true},
+    {id:3, title:"React", isDone: false},
+  ]
+
+  let tasks2: Array<TaskType> =[
+    {id:1, title:"Term", isDone: true},
+    {id:2, title:"XXX", isDone: false},
+    {id:3, title:"Jen", isDone: true},
+  ]
+
   return (
-    <div className="App">
-      hihiihi
+    <div>
+      <Todolist title="What to learn" tasks={tasks1} />
+      <Todolist title="Movies" tasks={tasks2}/>
     </div>
   );
 }
+
 
 export default App;
